4b4a3144b92b01c624ef85115624de678513b42f
bebob94 <bebo.macis@gmail.com>
2023-02-08 18:11:27 +0100

    add

--- src/Component/AllTheBooks.jsx
+++ src/Component/AllTheBooks.jsx
@@ -4,31 +4,50 @@ import { Form, Button } from "react-bootstrap";
 
 class AllTheBooks extends Component {
   state = {
-    title: null,
+    query: "",
   };
+
+  filterBookList(e) {
+    console.log(e.target.value);
+    this.setState({
+      query: e.target.value,
+    });
+  }
+
   render() {
     return (
       <>
         <Form>
           <Form.Group className="mb-3 ">
             <Form.Label>titolo Libro</Form.Label>
-            <Form.Control type="text" placeholder="inserisci il titolo" />
+            <Form.Control
+              type="text"
+              placeholder="inserisci il titolo"
+              onChange={(event) => this.filterBookList(event)}
+            />
           </Form.Group>
 
           <Button variant="primary" type="submit">
             Submit
           </Button>
         </Form>
-        {this.props.category.map((Book) => {
-          return (
-            <SingleBookCard
-              title={Book.title}
-              img={Book.img}
-              category={Book.category}
-              price={Book.price}
-            />
-          );
-        })}
+        {this.props.category
+          .filter((book) => {
+            return book.title
+              .toLowerCase()
+              .includes(this.state.query.toLowerCase());
+          })
+          .map((Book) => {
+            return (
+              <SingleBookCard
+                key={Book.asin}
+                title={Book.title}
+                img={Book.img}
+                category={Book.category}
+                price={Book.price}
+              />
+            );
+          })}
       </>
     );
   }
